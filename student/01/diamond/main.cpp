69629fd4c655c54b948268196de1103bf3ec2161
Mikko Kemppi <mikko.kemppi@tuni.fi>
2019-09-03 15:59:34 +0300

    pitäis toimia

--- student/01/diamond/main.cpp
+++ student/01/diamond/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    cout << "      *" << endl;
+    cout << "    *   *" << endl;
+    cout << "  *       *" << endl;
+    cout << "*           *" << endl;
+    cout << "  *       *" << endl;
+    cout << "    *   *" << endl;
+    cout << "      *" << endl;
     return 0;
 }

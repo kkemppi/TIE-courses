c79a81693d22ba86dd79b8d8b90698c5bf2a7cdf
Mikko Kemppi <mikko.kemppi@tuni.fi>
2019-09-08 18:49:28 +0300

    Syötteen lukeminen ja tulostus

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -4,4 +4,9 @@
 int main()
 {
     std::cout << "How many numbers would you like to have? ";
+    int amount = 0;
+    std::cin >> amount;
+    for ( int num = 1; num < amount+1; ++num ) {
+        std::cout << num << std::endl;
+    }
 }

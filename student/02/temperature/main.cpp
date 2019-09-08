e67b15667db4cca089748c9ed7928a7cfdc39084
Mikko Kemppi <mikko.kemppi@tuni.fi>
2019-09-08 18:36:16 +0300

    kirjoitusasun korjaus

--- student/02/temperature/main.cpp
+++ student/02/temperature/main.cpp
@@ -10,7 +10,7 @@ int main()
     double input_as_f = input * 1.8 + 32;
     double input_as_c = (input - 32) / 1.8;
     cout << input << " degrees Celsius is " << input_as_f << " degrees Fahrenheit" << endl;
-    cout << input << " degrees Fahrhenheit is " << input_as_c << " degrees Celsius" << endl;
+    cout << input << " degrees Fahrenheit is " << input_as_c << " degrees Celsius" << endl;
 
     return 0;
 }

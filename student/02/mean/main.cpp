06d22d1da18bfaf41e6a47ef8b96442d0329ea41
Mikko Kemppi <mikko.kemppi@tuni.fi>
2019-09-08 20:20:17 +0300

    virhetarkastelun lisäys

--- student/02/mean/main.cpp
+++ student/02/mean/main.cpp
@@ -8,13 +8,20 @@ int main()
     cin >> amount;
     double sum = 0;
     double avg = 0;
-    double value = 0;
-    for (int num = 0; num < amount; ++num ) {
-        cout << "Input " << num << ". number: ";
-        cin >> value;
-        sum = sum + value;
+    double value = 0; {
+        if (amount > 0) {
+            for (int num = 0; num < amount; ++num ) {
+                cout << "Input " << num << ". number: ";
+                cin >> value;
+                sum = sum + value;
+            }
+            avg = sum / amount;
+            cout << "Mean value of the given numbers is " << avg << endl;
+        }
+        else {
+            cout << "Cannot count mean value from " << amount << " numbers" << endl;
+        }
     }
-    avg = sum / amount;
-    cout << "Mean value of the given numbers is " << avg << endl;
+
 
 }

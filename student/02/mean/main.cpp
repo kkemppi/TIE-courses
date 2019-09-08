2f0d89d910dae763dd3f120f92ddc458d653397f
Mikko Kemppi <mikko.kemppi@tuni.fi>
2019-09-08 20:16:34 +0300

    perustoiminnallisuus toteutettu

--- student/02/mean/main.cpp
+++ student/02/mean/main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
-
-// Write here a function counting the mean value
+using namespace std;
 
 int main()
 {
-    std::cout << "From how many integer numbers you want to count the mean value? ";
+    cout << "From how many integer numbers you want to count the mean value? ";
+    int amount = 0;
+    cin >> amount;
+    double sum = 0;
+    double avg = 0;
+    double value = 0;
+    for (int num = 0; num < amount; ++num ) {
+        cout << "Input " << num << ". number: ";
+        cin >> value;
+        sum = sum + value;
+    }
+    avg = sum / amount;
+    cout << "Mean value of the given numbers is " << avg << endl;
+
 }

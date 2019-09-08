d57013d1c989da035fcfb651634c9c6f195829e4
Mikko Kemppi <mikko.kemppi@tuni.fi>
2019-09-08 19:31:10 +0300

    lisätty loppu toiminnallisuus

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -6,11 +6,17 @@ int main()
     cout << "How many numbers would you like to have? ";
     int amount = 0;
     cin >> amount;
+    int num = 1;
     {
-    for ( int num = 1; num < amount+1; ++num )
-        if ( num % 3 == 0)
+    for ( num = num; num < amount+1; ++num ) {
+        if ( num % 7 == 0 && num % 3 == 0)
+                    cout << "zip boing"<< endl;
+        else if ( num % 3 == 0)
             cout << "zip"<< endl;
+        else if ( num % 7 == 0)
+            cout << "boing"<< endl;
         else cout << num << endl;
+        }
     }
 }
 

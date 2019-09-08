fe6e3549f167480f381d3d066dc8f6bbd70bd14d
Mikko Kemppi <mikko.kemppi@tuni.fi>
2019-09-08 20:36:50 +0300

    kuution ensimmäinen palautus

---
+++ student/02/cube/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    cout << "Enter a number: ";
+    int value = 0;
+    cin >> value;
+    int cube = 0;
+    cube = pow(value, 3.0);
+    if (cube / value / value == value) {
+        cout << "The cube of " << value << " is " << cube << "." << endl;
+    }
+    else {
+        cout << "Error! The cube of " << value << " is not " << cube << "." << endl;
+    }
+}

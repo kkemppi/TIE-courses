e89df248ed7c35779ebb28b8a67b84519efdb618
Mikko Kemppi <mikko.kemppi@tuni.fi>
2019-09-08 20:45:22 +0300

    lisätty 0 tarkastelu

--- student/02/cube/main.cpp
+++ student/02/cube/main.cpp
@@ -8,11 +8,17 @@ int main()
     int value = 0;
     cin >> value;
     int cube = 0;
-    cube = pow(value, 3.0);
-    if (cube / value / value == value) {
-        cout << "The cube of " << value << " is " << cube << "." << endl;
+    if (value != 0) {
+        cube = pow(value, 3.0);
+        if (cube / value / value == value) {
+            cout << "The cube of " << value << " is " << cube << "." << endl;
+        }
+        else {
+            cout << "Error! The cube of " << value << " is not " << cube << "." << endl;
+
+        }
     }
     else {
-        cout << "Error! The cube of " << value << " is not " << cube << "." << endl;
+        cout << "The cube of 0 is 0." << endl;
     }
 }

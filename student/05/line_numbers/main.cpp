2580f1c15dc6e9f31a227bddfc08b014ac237f68
Mikko Kemppi <mikko.kemppi@tuni.fi>
2019-09-29 15:33:27 +0300

    Korjattu turhan tiedoston lisäys

--- student/05/line_numbers/main.cpp
+++ student/05/line_numbers/main.cpp
@@ -8,23 +8,21 @@ int main()
     cout << "Input file: ";
     string input_file;
     getline(cin, input_file);
+    ifstream input(input_file);
     cout << "Output file: ";
     string output_file;
     getline(cin, output_file);
-
-    ifstream input(input_file);
-    ofstream output(output_file);
     if (!input){
         cout << "Error! The file " << input_file << " cannot be opened." << endl;
         return EXIT_FAILURE;
-    }else{
-        string row;
-        int i = 1;
-        while (getline(input, row)){
-            output << i << " " << row << endl;
-            ++i;
-        }
-        input.close();
-        output.close();
     }
+    ofstream output(output_file);
+    string row;
+    int i = 1;
+    while (getline(input, row)){
+        output << i << " " << row << endl;
+        ++i;
+    }
+    input.close();
+    output.close();
 }

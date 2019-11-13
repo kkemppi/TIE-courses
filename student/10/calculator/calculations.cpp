e4b5bf100c1b35546457b9122d705b38cfb55132
Mikko Kemppi <mikko.kemppi@tuni.fi>
2019-11-13 18:30:20 +0200

    Eka palautus

---
+++ student/10/calculator/calculations.cpp
@@ -0,0 +1,26 @@
+// Note that there is no need for "using namespace std",
+// since no C++ standard libraries are used.
+#include <cmath>
+
+double addition(double left, double right) {
+    return left + right;
+}
+
+
+double subtraction(double left, double right) {
+    return left - right;
+}
+
+
+double multiplication(double left, double right) {
+    return left * right;
+}
+
+
+double division(double left, double right) {
+    return left / right;
+}
+
+double exponent(double left, double right){
+    return std::pow(left, right);
+}
